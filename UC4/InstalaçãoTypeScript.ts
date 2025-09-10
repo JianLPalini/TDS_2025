87d7151a417c9f979d2b6c6c890e2ec2f9a9cbf7
JianLPalini <jianpalini11@gmail.com>
2025-09-10 15:14:25 -0300

    Create InstalaçãoTypeScript.ts

---
+++ UC4/InstalaçãoTypeScript.ts
@@ -0,0 +1,21 @@
+//Caso não esteja padrão
+// Ir no flecha do lado do + no terminal - Select Default Profile - Selecionar Git Bash
+
+// Instalar comandos no terminal
+//  npm install typescript -D
+// npx tsc --init
+
+
+
+
+// Pasta ts.config.json
+
+{
+  "compilerOptions": {
+     "rootDir": "./src",
+     "outDir": "./dist",
+    "module": "commonjs",
+    "target": "es6",
+    "strict": true,
+  }
+}
