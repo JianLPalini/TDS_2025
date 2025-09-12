30ae23dc29c3f71c206c1f4363e84a5567aaa807
JianLPalini <jianpalini11@gmail.com>
2025-09-12 17:15:32 -0300

    Update Hospital.ts

--- UC4/Hospital.ts
+++ UC4/Hospital.ts
@@ -1,43 +1,52 @@
 // Atividade 1 -> Criar um sistema para um cliente leigo sem Jargão tecnico(Sistema Hospital)
+// Nome do grupo(Sidtopia)
 
 class Hospital{
     // Informações Paciente:
-    private nome_paciente:string;
+    private atendimento_registrado:number;
+    nome_paciente:string;
     private cpf_paciente:string;
     descricao_paciente:string;
+    alergia_paciente:string;
     idade_paciente:number;
     // Informação Medico:
-    nome_medico:string;
+    private nome_medico:string;
     idade_medico:number;
+    descricao_medico:string;
     area_medico:string;
     motivo_visita:string;
-    prescricao_medica:string;
-    medicamento:string;
 
     // Construtor para os itens do Hospital
-    constructor(nome_paciente:string,cpf_paciente:string,descricao_paciente:string,idade_paciente:number,nome_medico:string,idade_medico:number,area_medico:string,motivo_visita:string,prescricao_medica:string,medicamento:string){
-
+    constructor(nome_paciente:string,cpf_paciente:string,descricao_paciente:string,alergia_paciente:string,idade_paciente:number,nome_medico:string,idade_medico:number,descricao_medico:string,area_medico:string,motivo_visita:string){
+    
     // Referencia ao Objeto
+    this.atendimento_registrado = 0;
+    this.alergia_paciente = alergia_paciente;
     this.area_medico = area_medico;
     this.cpf_paciente = cpf_paciente;
+    this.descricao_paciente = descricao_medico;
     this.idade_medico = idade_medico;
     this.motivo_visita = motivo_visita;
     this.nome_medico = nome_medico;
     this.nome_paciente = nome_paciente;
     this.idade_paciente = idade_paciente;
     this.descricao_paciente = descricao_paciente;
-    this.prescricao_medica = prescricao_medica;
-    this.medicamento = medicamento;
+    this.descricao_medico = descricao_medico;
 
-        
     }
 
+    // Metodo Atendimento
     atendimento(){
         console.log(`Paciente ${this.nome_paciente} foi atendido por ${this.nome_medico}`)
     }
 
+    //Metodo Ficha
     ficha(){
         console.log(`Paciente ${this.nome_paciente} Foi feito a ficha, para ser atendido por ${this.nome_medico}`)
+    }
+    registrado_atendimento(){
+        this.atendimento_registrado =+1;
+        console.log(`Total de Atendimento: ${this.atendimento_registrado}`)
     }
 
      getNomePaciente(): string {
@@ -58,11 +67,12 @@ class Hospital{
 
 
 }
-
-const paciente1 = new Hospital("Jian Machado Silva","1212121212","Homem Adulto",26,"Oswaldo Pereira",39,"Clinico Geral","Dor na Coluna","Repouso e Hidratação","Dipirona");
-const paciente2 = new Hospital("Erica Macho Silva Lima","1212121212","Adolescente",17,"Luis Pereira Lima silva",59,"Clinico Geral","Dor na Coluna","Repouso e Hidratação","Dipirona");
-const paciente3 = new Hospital("Kalleo Pentecostal Pinto","1212121212","Adolescente",16,"Oswaldo Pereira",39,"Clinico Geral","Dor na Coluna","Repouso e Hidratação","Dipirona");
-const paciente4 = new Hospital("Sidnei Jacinto Biruleibe ","1212121212","Homem Adulto",27,"Luis Pereira Lima silva",59,"Clinico Geral","Dor na Coluna","Repouso e Hidratação","Dipirona");
+// Contruir Paciente 1
+const paciente1 = new Hospital("Jian Machado Silva","1212121212","Homem Adulto","Dipirona",25,"Oswaldo Pereira",39,"Homem Adulto","Clinico Geral","Dor na Coluna");
+// Contruir Paciente 2
+const paciente2 = new Hospital("Erika Macho Silva Lima","1212121212","Adolecente","Dipirona",25,"Luis Pereira Lima silva",59,"Homem Adulto","Clinico Geral","Dor na Coluna");
 
 paciente1.atendimento();
 paciente2.ficha();
+paciente1.registrado_atendimento();
+paciente2.registrado_atendimento();
