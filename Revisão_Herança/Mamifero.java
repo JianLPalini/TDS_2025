393497974875eb28f6645d8485db0e54f36f2313
JianLPalini <jianpalini11@gmail.com>
2025-11-04 16:54:58 -0300

    Create Mamifero class extending Animal

---
+++ Revisão_Herança/Mamifero.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author JIANLUCAPALINI
+ */
+public class Mamifero extends Animal{
+    private String corDoPelo;
+
+    public Mamifero(String corDoPelo, String raca, double peso) {
+        super(raca, peso);
+        this.corDoPelo = corDoPelo;
+    }
+    
+    
+}
