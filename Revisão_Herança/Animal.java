f74e58965f970e19bcebd9ca601e52b46f395033
JianLPalini <jianpalini11@gmail.com>
2025-11-04 16:54:33 -0300

    Add Animal class with attributes and method

---
+++ Revisão_Herança/Animal.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author JIANLUCAPALINI
+ */
+public class Animal {
+    private String raca;
+    private double peso;
+
+    public Animal(String raca, double peso) {
+        this.raca = raca;
+        this.peso = peso;
+    }
+    
+    public String mostrarRacaEPeso(){
+        return "Este animal é um(a) " + this.raca + " e pesa " + this.peso + "kg.";
+    }
+    
+    
+}
