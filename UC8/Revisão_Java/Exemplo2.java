dd0e84af3cf9e1cf9528ce568d27fca735e2cff0
JianLPalini <jianpalini11@gmail.com>
2025-11-04 16:53:23 -0300

    Create Exemplo2.java

---
+++ UC8/Revisão_Java/Exemplo2.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author JIANLUCAPALINI
+ */
+public class Pessoa {
+    // Atributos
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    // Métodos
+    public static void falar(){
+        System.out.println("A pessoa está falando");
+    }
+    
+    public void fraseIdade(){
+        System.out.println(this.nome + " tem " + this.idade + " anos.");
+    }
+
+    
+    
+  
+}
