a6c5ab0fb116c1631739bfab1cc6ae051d4e4db4
JianLPalini <jianpalini11@gmail.com>
2025-11-04 16:55:22 -0300

    Create Reptil class extending Animal

---
+++ Revisão_Herança/Reptil.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author JIANLUCAPALINI
+ */
+public class Reptil extends Animal{
+    private String corDasEscamas;
+
+    public Reptil(String corDasEscamas, String raca, double peso) {
+        super(raca, peso);
+        this.corDasEscamas = corDasEscamas;
+    }
+    
+}
