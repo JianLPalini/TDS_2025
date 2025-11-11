e48ec11bc981d67812a7f7816fb09d1d7c274adc
JianLPalini <jianpalini11@gmail.com>
2025-11-11 17:15:04 -0300

    Create Exercicio2.java

---
+++ UC8/Revisão_Java/Exercicio2.java
@@ -0,0 +1,11 @@
+public class Temperatura {
+    public static void main (String[]args){
+    double temperaturaC1 = 20;
+    double temperaturaC2 = 48;
+    double conversor1 = (temperaturaC1 * 1.8) + 32;
+    double conversor2 = (temperaturaC2 * 1.8) + 32;
+    
+        System.out.println("Temperatura está: " + conversor1);
+        System.out.println("Temperatura está: " + conversor2);
+    }
+}
