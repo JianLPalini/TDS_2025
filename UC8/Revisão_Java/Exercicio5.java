68e79e550cedef827366708ebc4dc74a505ed667
JianLPalini <jianpalini11@gmail.com>
2025-11-12 14:24:40 -0300

    Create Exercicio5.java

---
+++ UC8/Revisão_Java/Exercicio5.java
@@ -0,0 +1,10 @@
+public class Média {
+    public static void main(String[]args){
+    double n1 = 5;
+    double n2 = 10;
+    double n3 = 7;
+    double media = (n1+n2+n3 /3);
+    
+        System.out.println("A média do aluno é: " + media);
+    }
+}
