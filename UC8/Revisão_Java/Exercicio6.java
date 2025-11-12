d76f5dd1b33dc4e69f986069379d03bf21689456
JianLPalini <jianpalini11@gmail.com>
2025-11-12 14:26:57 -0300

    Create Exercicio6.java

---
+++ UC8/Revisão_Java/Exercicio6.java
@@ -0,0 +1,7 @@
+public class Números {
+    public static void main(String[]árgs){
+         int numero = 5;
+         String resultado = (numero%2 == 0)&& true? "Número é par!" : "Numero é impar!";
+            System.out.println(resultado);
+    }
+}
