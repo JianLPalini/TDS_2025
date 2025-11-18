ccefe78c181970ebc3f965d4a97a4c951e9385e5
JianLPalini <jianpalini11@gmail.com>
2025-11-18 16:42:01 -0300

    Create Atividade1.java

---
+++ UC8/AtividadesAnimes/Loops/Atividade1.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author JIANLUCAPALINI
+ */
+public class Anime13 {
+    public static void main(String[]args){
+        Scanner entrada = new Scanner(System.in);
+        int energia = 0;
+        
+        while (energia <= 50){
+               energia +=10;
+            System.out.println("Você está acumulando Energia!" + energia);
+
+        }
+    }
+}
